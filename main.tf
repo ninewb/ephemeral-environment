237d2f05ba6eda4c08ac3f03e4aa2b98192ef3e4
Nick Newbill <x@ferociouschipeater.com>
2024-04-16 16:54:58 -0400

    runner typo

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    tailscale = {
-      source = "tailscale/tailscale"
-      version = "0.13.5"
-    }
-  }
-}
+#terraform {
+#  required_providers {
+#    tailscale = {
+#      source = "tailscale/tailscale"
+#      version = "0.13.5"
+#    }
+#  }
+#}
 
 provider "cloudinit" {}
 
