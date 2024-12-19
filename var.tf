64c40e0388c624790003ea825c80339587d6b347
Nick Newbill <nick.newbill@sas.com>
2024-12-19 11:05:35 -0500

    Update vars to see if it takes

--- var.tf
+++ var.tf
@@ -10,6 +10,11 @@ provider "azurerm" {
   }
 }
 
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+
 variable "date" {
  type = string
 }
