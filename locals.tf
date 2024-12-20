7a3681c1ff81a59d21c81cefa64f8acb058a4438
Nick Newbill <nick.newbill@sas.com>
2024-12-20 08:30:10 -0500

    network_rg

--- locals.tf
+++ locals.tf
@@ -1,4 +1,13 @@
 locals {
   # CIDR/Network
   default_public_access_cidrs          = var.default_public_access_cidrs == null ? [] : var.default_public_access_cidrs
+ 
+  network_rg = (var.vnet_resource_group_name == null
+    ? local.aks_rg
+    : data.azurerm_resource_group.network_rg[0]
+  )
+
+  nsg         = var.nsg_name == null ? azurerm_network_security_group.nsg[0] : data.azurerm_network_security_group.nsg[0]
+  nsg_rg_name = local.network_rg.name
+
 }
