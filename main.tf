00a198e0abd8149f6c8b54adc22247943bd8ec0f
Nick Newbill <nick.newbill@sas.com>
2024-12-20 12:34:58 -0500

    test

--- main.tf
+++ main.tf
@@ -1,3 +1,25 @@
+provider "azurerm" {
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  partner_id      = var.partner_id
+  use_msi         = var.use_msi
+
+  features {    
+    virtual_machine {
+      delete_os_disk_on_deletion = true
+    }
+  }
+}
+
+provider "azuread" {
+  client_id     = var.client_id
+  client_secret = var.client_secret
+  tenant_id     = var.tenant_id
+}
+
 provider "cloudinit" {}
 
 data "azurerm_client_config" "current" {}
